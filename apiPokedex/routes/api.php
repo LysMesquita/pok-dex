1bd4206462f22878119c0de9d46245eab08f989a
Isabelly Victória <isabelly.mesquita@etec.sp.gov.br>
2025-09-05 22:07:18 -0300

    Update api.php

--- apiPokedex/routes/api.php
+++ apiPokedex/routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\PokemonsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // rotas para visualizar os registros
 Route::get('/',function(){return response()->json(['Sucesso'=>true]);});
-Route::get('/produtos',[ProdutosController::class,'index']);
-Route::get('/produtos/{codigo}',[ProdutosController::class,'show']);
+Route::get('/pokemons',[PokemonsController::class,'index']);
+Route::get('/pokemons/{codigo}',[PokemonsController::class,'show']);
 
-// rota para inserir os regiatros
-Route::post('/produtos',[ProdutosController::class,'store']);
+// rota para inserir os registros
+Route::post('/pokemons',[PokemonsController::class,'store']);
 
 // rota para alternar os registros
-Route::put('/produtos/{codigo}',[ProdutosController::class,'update']);
+Route::put('/pokemons/{codigo}',[PokemonsController::class,'update']);
 
 // rota para excluir o registro por id/codigo
-Route::delete('/produtos/{id}',[ProdutosController::class,'destroy']);
+Route::delete('/pokemons/{id}',[PokemonsController::class,'destroy']);
